0bc2bac528ddc225b36b7ed9ebe68419fd624131
Volodymir Zybin <v_admin@ukr.net>
2023-01-08 21:36:50 +0200

    Change static

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@ FROM python:3.10-alpine
 MAINTAINER Volodymir Zybin
 
 ENV PYTHONUNBUFFERED 1
-ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONDONTWRITEBYTECODE 1
 
 COPY requirements.txt /temp/requirements.txt
 COPY app /app
 WORKDIR /app
 EXPOSE 8000
+
+RUN pip install --upgrade pip
 RUN apk add postgresql-client build-base postgresql-dev
 RUN pip install -r /temp/requirements.txt
 RUN adduser --disabled-password user-app
+
 USER user-app
--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
+# DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
@@ -129,7 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
